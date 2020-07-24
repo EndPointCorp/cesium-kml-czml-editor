3c286a2d67ff890b8857146bb6dcccab017a0bb3
dkiselev <dmitry.v.kiselev@gmail.com>
2020-07-24 16:39:52 -0300

    Add validator for font value

--- js/fields/direct.js
+++ js/fields/direct.js
@@ -2,16 +2,30 @@ const template = `<v-text-field
     dense
     hide-details
     class="direct-property-field"
-    @input="handleUpdate"
-    v-model="entity[feature][field]"
-    :label="label">
-    </v-text-field>`;
+    @input="input"
+    :value="entity[feature][field]"
+    :label="label"
+    :rules="rules">
+</v-text-field>`;
 
 Vue.component('direct-field', {
-    props: ['entity', 'feature', 'field', 'label'],
+    props: ['entity', 'feature', 'field', 'label', 'rules', 'validator'],
     methods: {
+        input: function(value) {
+            if (this.validator) {
+                if (this.validator(value)) {
+                    this.entity[this.feature][this.field] = value;
+                    this.handleUpdate();
+                }
+            }
+            else {
+                this.entity[this.feature][this.field] = value;
+                this.handleUpdate();
+            }
+        },
         handleUpdate: function() {
             let val = undefined;
+
             if (this.entity[this.feature][this.field]) {
                 val = this.entity[this.feature][this.field].getValue();
             }
